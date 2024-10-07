62b0dc8a7bab7f625a9899afa9efdb11254f78e6
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2024-10-07 17:17:10 +0800

    HouseKeeping.java

--- HouseKeeping.java
+++ HouseKeeping.java
@@ -2,13 +2,13 @@ public class HouseKeeping implements HotelService //Service Class 2
 {
     public void cleanRoom(int roomNumber)
     {
-        roomNumber = 110;
-        System.out.println("Request to clean room number " + roomNumber);
+        //Implement request to clean a specific room number accordingly.
     }
 
     @Override
     public void manageHotelServices()
     {
+
         cleanRoom(110);
     }
 }
